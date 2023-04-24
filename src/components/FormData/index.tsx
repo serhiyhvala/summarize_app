15a8cfe1e5b2d1011414dbcecc86b471ff27154d
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-04-25 00:11:37 +0300

    Create Form Component

--- src/components/FormData/index.tsx
+++ src/components/FormData/index.tsx
@@ -1,12 +1,37 @@
 import InputField from '@components/InputField'
+import { useAppSelector } from '@hooks/useAppSelector.ts'
+import useCopyToClipboard from '@hooks/useCopyToClipboard.ts'
+import { useGetSummarizeQuery } from '@services/articleApi.ts'
+import Button from '@ui/Button'
 
 import styles from './formData.module.scss'
 
 const FormData = () => {
+	const { inputField } = useAppSelector(state => state.inputField)
+	const { data, isFetching, isError } = useGetSummarizeQuery(inputField, {
+		skip: inputField.length === 0
+	})
+	const [value, copy] = useCopyToClipboard()
+	const isCopied = value ? 'Copied!' : 'Copy'
+
 	return (
 		<section className={styles.formData}>
 			<InputField />
-			Data
+			{isFetching ? (
+				<h2 className={styles.state}>Loading...</h2>
+			) : isError ? (
+				<h2 className={styles.state}>Something went wrong</h2>
+			) : (
+				data?.summary && (
+					<>
+						<h2 className={styles.article}>
+							Article <span>Summary</span>
+						</h2>
+						<Button onClick={() => copy(data.summary)}>{isCopied}</Button>
+						<div className={styles.summary}>{data.summary}</div>
+					</>
+				)
+			)}
 		</section>
 	)
 }
