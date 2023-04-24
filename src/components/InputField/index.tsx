ee96c7b5b9a990bd59d3e04b305fc8b9ee9b8cc8
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-04-24 20:43:17 +0300

    Create Input Component

---
+++ src/components/InputField/index.tsx
@@ -0,0 +1,27 @@
+import { FormEvent, useState } from 'react'
+import { AiOutlineEnter } from 'react-icons/ai'
+
+import styles from './inputField.module.scss'
+
+const InputField = () => {
+	const [link, setLink] = useState('')
+	const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		setLink('')
+	}
+	return (
+		<form className={styles.inputField} onSubmit={handleSubmitForm}>
+			<input
+				type='url'
+				placeholder='Paste Your Link Here'
+				value={link}
+				onChange={e => setLink(e.target.value)}
+			/>
+			<button type='submit'>
+				<AiOutlineEnter size={20} />
+			</button>
+		</form>
+	)
+}
+
+export default InputField
